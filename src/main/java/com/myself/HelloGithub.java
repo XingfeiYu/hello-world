e6feed9264323d86235d710060f706c8695f0aff
xingfeiy <yxfly@hotmail.com>
2016-08-26 15:03:00 -0700

    Add hello class
    Hello

---
+++ src/main/java/com/myself/HelloGithub.java
@@ -0,0 +1,10 @@
+package com.myself;
+
+/**
+ * Created by xingfeiy on 8/26/16.
+ */
+public class HelloGithub {
+    public static void main(String[] args) {
+        System.out.println("Hello Github!");
+    }
+}
